9c7bf43a1e78152d8d26d7c338eeb4cee611c8ee
Richard Guo <97694222+richrichierich@users.noreply.github.com>
2024-11-28 17:55:24 -0500

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -1,8 +1,10 @@
 -- Q1
 
-SELECT a.a_id, a.name, COUNT(t.pass_id)
-FROM Airport a JOIN Flight f ON a.a_id = f.a_id
+SELECT a1.a_id, a.name, COUNT(t.pass_id)
+FROM Flight f JOIN Route r ON f.f_id = r.r_id
+JOIN Airport a1 ON a1.a_id = r.departure_airport
+JOIN Airport a2 ON a2.a_id = r.arrival_airport
 JOIN ScheduledFlight sf ON f.f_id = sf.f_id
 JOIN Ticket t ON sf.t_id = t.t_id
 WHERE YEAR(f.day) = 2023
-GROUP BY a.a_id, a.name;
+GROUP BY a1.a_id, a1.name;
