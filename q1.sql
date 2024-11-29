8606d4f283212f0ca396e97742b7ae5e79edc21c
Richard Guo <97694222+richrichierich@users.noreply.github.com>
2024-11-28 19:58:54 -0500

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -9,7 +9,7 @@ FROM Flight f JOIN Route r ON f.f_id = r.r_id
 JOIN Airport a1 ON a1.a_id = r.departure_airport
 JOIN ScheduledFlight sf ON f.f_id = sf.f_id
 JOIN Ticket t ON sf.t_id = t.t_id
-WHERE YEAR(f.day) = 2023
+WHERE EXTRACT(YEAR FROM f.day) = 2023
 GROUP BY a1.a_id, a1.name;
 
 CREATE VIEW Arrivals AS 
@@ -18,7 +18,7 @@ FROM Flight f JOIN Route r ON f.f_id = r.r_id
 JOIN Airport a2 ON a2.a_id = r.arrival_airport
 JOIN ScheduledFlight sf ON f.f_id = sf.f_id
 JOIN Ticket t ON sf.t_id = t.t_id
-WHERE YEAR(f.day) = 2023
+WHERE EXTRACT (YEAR FROM f.day) = 2023
 GROUP BY a2.a_id, a2.name;
 
 SELECT a_id, name, SUM(count)
