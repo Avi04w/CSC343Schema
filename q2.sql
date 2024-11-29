1732823797811f59b4a1b1e7abcad3a9472c00b4
Avi Walia <78369627+Avi04w@users.noreply.github.com>
2024-11-28 20:55:36 -0500

    Update q2.sql

--- q2.sql
+++ q2.sql
@@ -2,13 +2,13 @@ DROP VIEW IF EXISTS RouteFrequency CASCADE;
 DROP VIEW IF EXISTS MaxRoutes CASCADE;
 
 CREATE VIEW RouteFrequency AS 
-    SELECT p.name, r.r_id, COUNT(*) AS count
+    SELECT p.full_name, r.r_id, COUNT(*) AS count
     FROM Flight f JOIN Route r ON f.r_id = r.r_id
     JOIN ScheduledFlight sf ON f.f_id = sf.f_id
-    JOIN Ticket t ON t.f_id = sf.f_id
+    JOIN Ticket t ON t.t_id = sf.t_id
     JOIN Passenger p ON t.pass_id = p.pass_id
     WHERE EXTRACT (YEAR FROM f.day) = 2023
-    GROUP BY p.name, f.r_id
+    GROUP BY p.full_name, r.r_id
 ;
 
 CREATE VIEW MaxRoutes AS
@@ -16,5 +16,5 @@ SELECT r_id, MAX(count) AS max
 FROM RouteFrequency
 GROUP BY r_id;
 
-SELECT rf.name, rf.r_id, rf.count
+SELECT rf.full_name, rf.r_id, rf.count
 FROM RouteFrequency rf JOIN MaxRoutes mr ON rf.r_id = mr.r_id AND rf.count = mr.max;
