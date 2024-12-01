51d06da9180cd9b3b7f18b1a1214767e71081240
Richard Guo <97694222+richrichierich@users.noreply.github.com>
2024-11-30 19:17:22 -0500

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -1,28 +1,26 @@
-DROP VIEW IF EXISTS CountryRoutes CASCADE;
+-- Assuming that the average passengers requiring visas between country A and country B is calculated by # people requiring visas / # of flights
+DROP VIEW IF EXISTS PairAverages CASCADE;
 DROP VIEW IF EXISTS GlobalAverage CASCADE;
-DROP VIEW IF EXISTS FilteredRoutes CASCADE;
 
-CREATE VIEW CountryRoutes AS 
-    SELECT r.r_id, a1.country AS departure_country, a2.country AS arrival_country, AVERAGE(COUNT(v.pass_id)) AS avg_visa_passengers
-    FROM Flight f JOIN Route r ON f.r_id = r.r_id
-    JOIN Airport a1 ON r.departure_airport = a1.a_id
-    JOIN Airport a2 ON r.arrival_airport = a2.a_id
-    JOIN ScheduledFlight sf ON f.f_id = sf.f_id
-    JOIN Ticket t ON sf.t_id = t.t_id
-    JOIN Visas v ON t.pass_id = v.pass_id 
-    WHERE a1.country <> a2.country AND v.visa = a2.country
-    GROUP BY r.r_id, a1.country, a2.country;
 
-CREATE VIEW GlobalAverage AS 
-    SELECT AVG(avg_visa_passengers) AS overall_avg_visa_passengers FROM CountryRoutes;
+CREATE VIEW PairAverages AS (
+	SELECT a1.country AS country1, a2.country AS c2, COUNT(p.pass_id) / COUNT(f.f_id) AS avg
+	FROM Flight f JOIN Route r ON f.r_id = f.r_id
+	JOIN Airport a1 ON r.departure_airport = a1.a_id
+	JOIN Airport a2 ON r.arrival_airport = a2.a_id
+	JOIN ScheduledFlight sf ON f.f_id = sf.f_id
+	JOIN Ticket t ON sf.t_id = t.t_id
+	JOIN Passenger p ON t.pass_id = p.pass_id
+	JOIN Visas v ON v.pass_id = p.pass_id
+	WHERE a1.country <> a2.country AND v.visa = a2.country
+	GROUP BY a1.country, a2.country
+);
 
-CREATE VIEW FilteredRoutes AS 
-    SELECT cr.r_id, cr.departure_country, cr.arrival_country, cr.avg_visa_passengers
-    FROM CountryRoutes cr CROSS JOIN GlobalAverage ga 
-    WHERE cr.avg_visa_passengers < 0.25 * ga.overall_avg_visa_passengers;
+CREATE VIEW GlobalAverage AS (
+	SELECT AVERAGE(avg)
+	FROM PairAverages;
+);
 
-SELECT a1.name, a2.name, fr.avg_visa_passengers
-FROM FilteredRoutes fr JOIN Route r ON fr.r_id = r.r_id
-JOIN Airport a1 ON r.departure_airport = a1.a_id
-JOIN Airport a2 ON r.arrival_airport = a2.a_id;
-
+SELECT pa.c1, pa.c2, pa.avg
+FROM PairAverages pa
+WHERE pa.avg >= (SELECT * FROM GlobalAverage);
